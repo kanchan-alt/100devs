7386da9cb07d23d986fb4a616c5fd1f0ef957f15
kanchanlatakujur <kanchanlatakujur41@gmail.com>
2023-07-07 15:23:55 +0530

    review-in-paradise-after-paradise Done

---
+++ class20-materials /review-in-paradise-after-paradise/js/main.js
@@ -0,0 +1,18 @@
+// Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+
+function fistLast(numbers ){
+    if(numbers[0] < numbers[numbers.length -1]){
+        alert("Hi")
+    }else if(numbers[0] > numbers[number.length-1])
+    {
+        alert("Bye")
+}else if(numbers[0] = numbers[numbers.length -1]){
+    alert("We close in an hour")
+}
+
+}
+
+
+let numbers = [8,9,10,4]
+fistLast(numbers);
